50c0b7ef5f3ec206584e9eccce5e53a558c4c471
dripminister <95714820+dripminister@users.noreply.github.com>
2023-03-13 11:01:43 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCadA_KI17q7jC8DaOmhSm_C4MnJRIKtyg",
-	authDomain: "reddit-clone-fa15f.firebaseapp.com",
-	projectId: "reddit-clone-fa15f",
-	storageBucket: "reddit-clone-fa15f.appspot.com",
-	messagingSenderId: "1059061387454",
-	appId: "1:1059061387454:web:cc003b35cb875d7d7dceef",
+ // Firebase config
 }
 
 const app = initializeApp(firebaseConfig)
